fd5dc0a8c9903c7e8be214d46e50f53be856ad4f
Leidy <leidyjoanne86@gmail.com>
2021-04-08 21:15:45 -0500

    deploy hekoru

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {CommonModule} from '@angular/common';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'landing', pathMatch: 'full'},
+  {path: '', redirectTo: 'landing/overview', pathMatch: 'full'},
 ];
 
 @NgModule({
